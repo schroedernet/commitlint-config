dac3902e4596d0c8a721f4255c01d7b5c420eada
John Schroeder <john@schroedernet.software>
2020-11-30 01:14:59 -0600

    ci(test): Configure junit and cobertura reports

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,13 @@
 
 export default {
   collectCoverage: true,
-  coverageDirectory: 'tmp/test/coverage',
+  coverageDirectory: 'tmp/test/reports/coverage',
   coverageProvider: 'v8',
+  coverageReporters: ['cobertura', 'lcov', 'text'],
   preset: 'ts-jest',
+  reporters: [
+    'default',
+    ['jest-junit', {outputDirectory: 'tmp/test/reports/unit'}],
+  ],
   testEnvironment: 'node',
 }
